0c079a9f3cb9334fe48e7bed81d7edfd094052ea
lisa-adr <158718968+lisa-adr@users.noreply.github.com>
2024-02-03 09:42:20 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,21 +3,21 @@ module.exports = {
   Prefix: "?",
 
   Users: {
-    DEVELOPERS: ["803678553988268042"],
+    DEVELOPERS: ["   YOUR  USER     ID   "],
   },
 
   Client: {
-    TOKEN: "MTIwMTk4MTM0OTU3ODQwNzk2Ng.GJOme_.Op82NRsDg_CmwZyPnuHPcZaP-iGfbXYIiX0VrI",
-    ID: "1201981349578407966"
+    TOKEN: "    BOT     TOKEN",
+    ID: "  BOT ID   "
   },
 
   Link: {
     SUPPORT: "https://discord.gg/",
-    INVITE: "https://discord.com/api/oauth2/authorize?client_id=1201981349578407966&permissions=8&scope=applications.commands%20bot"
+    INVITE: "https://discord.com/api/oauth2/authorize?client_id=    "CLIENTID"   &permissions=8&scope=applications.commands%20bot"
   },
 
   Embed: {
     COLOR: "2b2d31",
-    IMAGE: "https://i1.sndcdn.com/artworks-pi1ULT06Lyht9r9m-nVwnyw-t500x500.jpg"
+    IMAGE: ""
   }
 }
